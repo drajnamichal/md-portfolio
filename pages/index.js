ac3c068718f59f77cd0af530cd60b06a44320544
Michal Drajna <michal.drajna@investown.cz>
2023-07-25 09:08:48 +0200

    links added

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,14 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+                <a 
+                  href="/resume.pdf" 
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" 
+                  download
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
           <div className="text-center p-10">
@@ -45,9 +52,15 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
-            <AiFillFacebook />
-            <AiFillLinkedin />
-            <AiFillGithub />
+            <a href="https://www.facebook.com/michaldrajna/">
+              <AiFillFacebook />
+            </a>
+            <a href="https://www.linkedin.com/in/michaldrajna-qa/">
+              <AiFillLinkedin />
+            </a>
+            <a href="https://github.com/drajnamichal">
+              <AiFillGithub />
+            </a>
             <AiFillMail />
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
