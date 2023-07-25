f92887ff6442f0f24e6db14e8db7cc5aa79b1312
Michal Drajna <michal.drajna@investown.cz>
2023-07-25 10:02:24 +0200

    update image

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
             <AiFillMail />
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-            <Image alt="emoji" src={drajnica} layout="fill" objectFit="cover"/>
+            <Image alt="emoji" src="/drajnica.png" layout="fill" objectFit="cover"/>
           </div>
         </section>
 
