8635fa50cfb674e4a9ee3a90feff5d23de435075
Michal Drajna <michal.drajna@investown.cz>
2023-07-25 13:47:04 +0200

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
