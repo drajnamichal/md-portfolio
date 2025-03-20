337f819a5fcd426b16f696f98b705501a4088a19
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-20 12:28:48 +0100

    style: rearrange footer elements and update copyright text

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,7 @@ import Typewriter from 'typewriter-effect';
 export default function Footer() {
   return (
     <footer className="py-16 mt-16 text-center text-gray-400 dark:text-gray-500">
-      <div className="space-y-2">
-        <p>
-          Copyright © <span className="text-gray-600 dark:text-gray-400">2025</span> Michal Drajna
-        </p>
+      <div className="space-y-4">
         <div className="text-sm font-burtons h-6 flex justify-center items-center">
           <Typewriter
             options={{
@@ -18,6 +15,9 @@ export default function Footer() {
             }}
           />
         </div>
+        <p>
+          Copyright © <span className="text-gray-400 dark:text-gray-500">2025</span>
+        </p>
       </div>
     </footer>
   );
