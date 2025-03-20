6a049ea87324e97abea5c383fcf9e779c525318e
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-20 19:08:59 +0100

    feat: version 3

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,8 @@ export default function Home() {
       <section className="min-h-screen relative">
         <div className="absolute inset-0" style={{
           backgroundImage: 'radial-gradient(circle at 1px 1px, rgba(0, 128, 128, 0.1) 1px, transparent 0)',
-          backgroundSize: '40px 40px'
+          backgroundSize: '40px 40px',
+          zIndex: 0
         }} />
         <motion.div 
           className="text-center p-10 relative"
