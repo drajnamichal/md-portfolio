804a93884fcc1b34b01779ff946de004102ac308
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-20 13:02:10 +0100

    update: show 2 reviews per page in slider

--- pages/courses.js
+++ pages/courses.js
@@ -70,7 +70,7 @@ export default function Courses() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
