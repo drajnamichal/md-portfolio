0c072cf22b1392b80e8da9154de2eb8c5b802fc4
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-20 10:57:10 +0100

    style: lighten copyright text color

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-16 mt-16">
-      <div className="text-center text-gray-600 dark:text-gray-400">
+      <div className="text-center text-gray-400 dark:text-gray-500">
         <p>Copyright © 2025 Michal Drajna</p>
       </div>
     </footer>
